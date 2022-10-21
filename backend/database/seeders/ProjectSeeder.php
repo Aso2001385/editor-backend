7c9d051b5a450943877f14488e2f7d9db017f062
[goto] <[2001163@s.asojuku.ac.jp]>
2022-10-21 11:07:00 +0900

    ProjectSeeder

--- backend/database/seeders/ProjectSeeder.php
+++ backend/database/seeders/ProjectSeeder.php
@@ -17,8 +17,8 @@ class ProjectSeeder extends Seeder
         DB::table('projects')->insert([
             'user_id' => rand(1,10),
             'name' => Str::random(10),
-            'updated_at' => now(),
-            'created_at' => now(),
+            'updated_at' => date('Y-m-d H:i:s'),
+            'created_at' => date('Y-m-d H:i:s'),
         ]);
 
     }
