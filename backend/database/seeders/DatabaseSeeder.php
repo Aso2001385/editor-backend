c480fdf81054aea17bbd9a270c6477ac6b1f4861
[goto] <[2001163@s.asojuku.ac.jp]>
2022-11-18 11:06:44 +0900

    1118_中間テーブルを含めたseeder作成

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Project;
 use App\Models\Design;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,30 +17,44 @@ class DatabaseSeeder extends Seeder
     public function run() {
 
    // $user = User::factory(5)
-   // ->has(Project::factory(2)
-   // ->has(Design::factory(2)),'projects','designs')
+   // ->has(Project::factory(3)
+   // ->has(Design::factory(3)),'projects','designs')
+    //->create();
+
+    // $user = User::factory(5)
+    // ->has(Design::factory(3),'designs')
+    // ->create();
+
+ //   $user = User::factory(5)
+  //  ->has(Project::factory(3)
+  //  ->has(Design::factory(3))
+ //   ->state(
+ //       function(array $attributes, User $user) {
+ //           return ['id' => $user->type];
+ //       }))
+  //      ->create();
+
+//$user = User::factory(5)
+  //  ->has(Design::factory(3)
+  //  ->state(
+   //     function(array $attributes, User $user) {
+   //         return ['id' => $user->type];
+   //     }))
    // ->create();
 
-   // $user = User::factory()
-   // ->has(Design::factory(5),'designs')
-   // ->create();
+    factory(User::class,10)->create()->each(function($user){
+        factory(Project::class)->create([
+            'user_id'=>$user->id
 
-    $user = User::factory(5)
-    ->has(Project::factory(3)
-    ->has(Design::factory(3))
-    ->state(
-        function(array $attributes, User $user) {
-            return ['id' => $user->type];
-        }))
-        ->create();
+        ]);
+        factory(Design::class)->create([
+            'user_id'=>$user->id
 
-$user = User::factory(5)
-    ->has(Design::factory(3)
-    ->state(
-        function(array $attributes, User $user) {
-            return ['id' => $user->type];
-        }))
-    ->create();
+        ]);
+    });
+
+
+
 
     }
 }
