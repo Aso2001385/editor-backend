ba291e865bf5922f867de4c1806d0d43f88a28d6
[Aso2001195] <[2001195@s.asojuku.ac.jp]>
2022-12-20 18:42:28 +0900

    再送信修正

--- backend/app/Http/Controllers/VerificationController.php
+++ backend/app/Http/Controllers/VerificationController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
 use App\Models\User;
 use App\Models\UserVerifications;
+use App\Mail\VerificationMail;
 
 
 class VerificationController extends Controller
@@ -24,35 +25,29 @@ class VerificationController extends Controller
     {
         $verification_info=UserVerifications::where('email','=',$request['email'])->first();
         if($verification_info!=null && $verification_info['code'] == $request['code']){
-            User::create([
+            $user = User::create([
                 'name'=>$verification_info['name'],
                 'email'=>$verification_info['email'],
                 'password'=>$verification_info['password'],
             ]);
 
             $verification_info->forceDelete();
-            return response()->json(true, Response::HTTP_OK);
+            return response()->json($user, Response::HTTP_OK);
         }
 
         // return response()->json($verification_info, Response::HTTP_OK);
-        return response()->json('CODEが間違っています　再度入力してください', Response::HTTP_UNPROCESSABLE_ENTITY);
+        return response()->json('CODEが間違っています 再度入力してください', Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 
     public function reSend($email)
     {
-        $verification_info=UserVerifications::where('email','=',$email)->first();
-        if($verification_info['code'] == $request['code'] && isset($verification_info)){
-            User::create([
-                'name'=>$verification_info['name'],
-                'email'=>$verification_info['email'],
-                'password'=>$verification_info['password'],
-            ]);
-
-            $verification_info->forceDelete();
-            return response()->json(true, Response::HTTP_OK);
-        }
-
-        // return response()->json($verification_info, Response::HTTP_OK);
-        return response()->json('CODEが間違っています　再度入力してください', Response::HTTP_UNPROCESSABLE_ENTITY);
+    
+        $code=str_pad(random_int(0,999999),6,0, STR_PAD_LEFT);
+        $userVerification=UserVerifications::updateOrCreate(['email'=>$email],['code'=>$code]);
+        $mail_address = $email;
+        $name = $userVerification->name;
+        $text = $userVerification->name."さんのcodeは".$userVerification->code."です。";
+        Mail::to($mail_address)->send(new VerificationMail($name,$text));
+        return response()->json(['emial'=>$email], Response::HTTP_OK);
     }
 }
