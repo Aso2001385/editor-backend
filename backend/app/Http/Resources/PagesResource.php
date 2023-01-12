f5bd5bf468f365b0c84341c36b47cfdf56c88217
Aso2001385 <2001385@s.asojuku.ac.jp>
2023-01-12 09:20:05 +0900

    ページ一覧の保存更新諸々

--- backend/app/Http/Resources/PagesResource.php
+++ backend/app/Http/Resources/PagesResource.php
@@ -6,7 +6,7 @@ use App\Models\Design;
 use App\Models\Project;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PageResource extends JsonResource
+class PagesResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -20,11 +20,9 @@ class PageResource extends JsonResource
         return [
             'id' => $this->id,
             'project_uuid' => Project::find($this->project_id)->uuid,
-            'number' => $this->number,
-            'user_id' => $this->user_id,
             'design_uuid' => Design::find($this->design_id)->uuid,
+            'number' => $this->number,
             'title' => $this->title,
-            'contents' => $this->contents,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
