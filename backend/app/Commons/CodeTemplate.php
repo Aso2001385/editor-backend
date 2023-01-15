04792bdbc5738e63388639a2048de25a4323366f
Aso2001385 <2001385@s.asojuku.ac.jp>
2023-01-16 01:08:45 +0900

    修正

--- backend/app/Commons/CodeTemplate.php
+++ backend/app/Commons/CodeTemplate.php
@@ -9,50 +9,50 @@ class CodeTemplate{
     public static function htmlSet($text,$title,$design_name,$lis){
 
         $html = <<<__HTML__
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-              <meta charset="UTF-8">
-              <meta http-equiv="X-UA-Compatible" content="IE=edge">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <link rel="stylesheet" href="./css/sanitize.css" />
-              <link rel="stylesheet" href="./css/variable.css" />
-              <link rel="stylesheet" href="./css/parts/menu.css" />
-              <link rel="stylesheet" href="./css/parts/slide.css" />
-              <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-              <link rel="icon" href="./assets/images/language-html5.png" type="image/png" sizes="24x24">
-              <title>$title</title>
-              <script type="module">
-                import { styleSetter } from "./js/design-setting.js";
-                import design from "./assets/designs/$design_name.json" assert { type: "json" };
-                styleSetter(design);
-              </script>
-            </head>
-            <body>
-              <header class="header">
-                <button class="burger-btn">
-                  <span class="bar bar_top"></span>
-                  <span class="bar bar_mid"></span>
-                  <span class="bar bar_bottom"></span>
-                </button>
-                <div class="nav-wrapper">
-                  <nav class="header-nav">
-                    <ul class="nav-list">
-                      $lis
-                    </ul>
-                  </nav>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="./css/sanitize.css" />
+            <link rel="stylesheet" href="./css/variable.css" />
+            <link rel="stylesheet" href="./css/parts/menu.css" />
+            <link rel="stylesheet" href="./css/parts/slide.css" />
+            <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+            <link rel="icon" href="./assets/images/language-html5.png" type="image/png" sizes="24x24">
+            <title>$title</title>
+            <script type="module">
+            import { styleSetter } from "./js/design-setting.js";
+            import design from "./assets/designs/$design_name.json" assert { type: "json" };
+            styleSetter(design);
+            </script>
+        </head>
+        <body>
+            <header class="header">
+            <button class="burger-btn">
+                <span class="bar bar_top"></span>
+                <span class="bar bar_mid"></span>
+                <span class="bar bar_bottom"></span>
+            </button>
+            <div class="nav-wrapper">
+                <nav class="header-nav">
+                <ul class="nav-list">
+                    $lis
+                </ul>
+                </nav>
+            </div>
+            </header>
+            <div style="height: 90vh; overflow-y: scroll">
+                <div id="contents">
+                $text
                 </div>
-              </header>
-              <div style="height: 90vh; overflow-y: scroll">
-                  <div id="contents">
-                    $text
-                  </div>
-              </div>
-              <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/js/swiper.min.js"></script>
-              <script src="/js/parts/slide.js"></script>
-              <script src="/js/parts/header.js"></script>
-            </body>
-            </html>
+            </div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/js/swiper.min.js"></script>
+            <script src="/js/parts/slide.js"></script>
+            <script src="/js/parts/header.js"></script>
+        </body>
+        </html>
         __HTML__;
 
         return $html;
--- backend/app/Commons/DesignCommon.php
+++ backend/app/Commons/DesignCommon.php
@@ -2,10 +2,12 @@
 
 namespace App\Commons;
 
+use Illuminate\Support\Facades\Storage;
+
 class DesignCommon{
 
     public static function design(){
-        return Storage::get('previews/templates/presetJs.txt');
+        return Storage::get('previews/templates/preset.json');
     }
     public static function preview(){
         return Storage::get('previews/templates/preset.txt');
